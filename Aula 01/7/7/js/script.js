8017d308b408627102a2a4636e5b8b0770bdb879
Manotakas <takedi006@gmail.com>
2024-08-06 14:26:41 -0300

    Coloca os exercicios das Olimpiadas no repositorio

---
+++ Aula 01/7/7/js/script.js
@@ -0,0 +1,12 @@
+var preco = parseFloat(prompt("digite o preço do ingresso: "));
+var Receita = -3*(preco*preco)+(180*preco)
+
+var precoReal = preco*5.65
+
+console.log("1= ",preco)
+console.log("2= ",Receita)
+
+console.log("em real é: ")
+
+console.log("1= ",precoReal)
+console.log("2= ",Receita*5.65)
