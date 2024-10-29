d92e645dd30cc214859fc0351e527fca92382979
Manotakas <takedi006@gmail.com>
2024-10-29 14:37:28 -0300

    lop

---
+++ Aula 06/exercicio laço.php
@@ -0,0 +1,18 @@
+<?php
+
+$numeros = array(2, 5, 7, 8, 10, 18, 30, 37, 38, 60);
+
+foreach ($numeros as $numero){
+    
+
+    if ($numero % 2 == 0){
+        echo " $numero é par <br>";
+    }
+
+    else if ($numero % 2 != 0 ){
+        echo " $numero é impar <br>";
+    }
+
+}
+
+?>
